81c279518412f1b89a26d3ba11baeca3fa658a0a
Hoang Quoc Cuong <hoangquoccuong05th@gmail.com>
2025-11-22 10:01:42 +0700

    restructure and adding parsers

--- parser/extracting.py
+++ parser/extracting.py
@@ -17,13 +17,25 @@ articles = re.findall(pattern_correct, text, flags=re.DOTALL)
 articles[-1]+="Bộ luật Hình sự hết hiệu lực thi hành kể từ ngày Bộ luật này có hiệu lực thi hành."
 
 # process article titles
+
+metadata = []
 for article in articles:
+    law_and_content = {}
+    article = re.sub(r"\nChương\s+[IVXLCDM]+.*?\Z",'', article, flags=re.DOTALL)
     raw_title = re.findall(r"Điều\s+\d+\..*?(?=\n)", article)
     if len(raw_title)>0:
         raw_title_old = re.findall(r"Điều\s+\d+\..*?(?=\n)", article)[0]
         processed_title = re.sub(r"\s*\[.*?\]", '', raw_title_old)        
-        print(processed_title)
     else: 
-        print("null")
+        processed_title = "null"
+    if processed_title != "null":
+        law_and_content['title'] = processed_title
+        law_and_content['content'] = article.replace(raw_title_old, '')
+        metadata.append(law_and_content)
 
-print (len(articles))
+# save metadata to a file
+import json
+output_file_path = os.path.join(base_directory,'parser','luat_hinh_su_metadata.json')
+with open(output_file_path, 'w', encoding='utf-8') as f:
+    json.dump(metadata, f, ensure_ascii=False, indent=4)
+
