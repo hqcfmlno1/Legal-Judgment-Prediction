f3cbc38aa64bfd31e5993a75d58e000251e140e7
Hoang Quoc Cuong <hoangquoccuong05th@gmail.com>
2025-11-25 09:31:00 +0700

    add graph builder used for graph lookup

---
+++ chunking/graph_builder.py
@@ -0,0 +1,68 @@
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+import json
+from pydantic import BaseModel, Field
+
+
+base_directory = os.path.dirname(os.path.abspath(__file__))
+json_file_path = os.path.join(base_directory,'data','processed','luat_hinh_su_metadata.json')
+dotenv_path = os.path.join(base_directory,'.env')  
+load_dotenv(dotenv_path=dotenv_path)
+
+with open(json_file_path, 'r', encoding='utf-8') as f:
+    metadata = json.load(f)
+
+
+class content_one_article(BaseModel):
+    id: int = Field(description="id của điều luật chính là số thứ tự của điều luật đó và là số nguyên")
+    referenced_laws: list[int] = Field(description="danh sách các điều luật được đề cập trong điều luật hiện tại")
+
+class content_response(BaseModel):
+    items : list[content_one_article]
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+generation_config = {
+    "temperature": 0.0,   
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 10000, 
+    "response_mime_type": "application/json",
+    "response_schema" : content_response
+    }
+model = genai.GenerativeModel(
+model_name="gemini-2.5-flash",
+    generation_config=generation_config
+    )
+
+
+def list_of_referenced_laws(law_list):
+    prompt = f"""
+    tôi có một đoạn văn bản pháp luật chứa thông tin đầy đủ của các điều luật như sau: {law_list} và mỗi điểu luật có id chính là số thứ tự của điều luật đó ví dụ điều 36 có id là 36
+    tôi cần bạn tạo giúp tôi 1 kiểu dữ liệu list của các dict trong python trong đó key là id của điều luật hiện tại và value là list của các điều luật được đề cập đến trong điều luật hiện tại
+    nhớ là bạn chỉ được cung cấp cho tôi theo định dạng list của các dict trong python thôi không được thêm bất kỳ lời giải thích nào khác ngoài định dạng dict đó, nếu điều luật hiện tại không đề cập đến điều luật khác thì đưa ra value là list rỗng, tuyệt đối chỉ đưa ra định dạng tôi đề cập không được thêm bất kì thông tin nào khác output chỉ được có dạng '{{[{{id:val}}]}}' thôi KHÔNG ĐƯỢC THÊM KÍ HIỆU HOẶC CÁI GÌ KHÁC ĐỊNH DẠNG TÔI MUỐN OUTPUT CHỈ 1 DÒNG THÔI VÀ ĐỊNH DẠNG ĐÓ PHẢI CÓ THỂ DÙNG ĐƯỢC TRONG HÀM LOADS CỦA JSON PYTHON, GIÁ TRỊ CỦA KEY LÀ INT, CHÚ Ý dấu nháy đơn là ' KHÔNG PHẢI LÀ ` làm ơn output là 1 dòng để nếu tôi sử dụng output này cho hàm loads thì sử dụng được
+    """
+    response = model.generate_content(prompt)
+    return response.text
+    
+
+graph_list = []
+for i in range(0,len(metadata),10):
+    if i!=0:
+        law_list = [article['title']+article['content'] for article in metadata[i-10:i]]
+        law_list = "\n\n".join(law_list)
+        graph_list += json.loads(list_of_referenced_laws(law_list))['items']
+
+final_index = int((len(metadata)-1)/10)*10
+if final_index!=len(metadata)-1:
+    law_list = [article['title']+article['content'] for article in metadata[final_index:len(metadata)]]
+    law_list = "\n\n".join(law_list)
+    graph_list += json.loads(list_of_referenced_laws(law_list))['items']
+else:
+    law_list = [article['title']+article['content'] for article in metadata[final_index]]
+    law_list = "\n\n".join(law_list)
+    graph_list += json.loads(list_of_referenced_laws(law_list))['items']
+
+output_file_path_meta = os.path.join(base_directory,'data','processed','graph_lookup.json')
+with open(output_file_path_meta, 'w', encoding='utf-8') as f:
+    json.dump(graph_list, f, ensure_ascii=False, indent=4)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import psycopg2
 import torch
 from pyvi import ViTokenizer
 from transformers import AutoTokenizer, AutoModel
+import google.generativeai as genai
 
 base_directory = os.path.dirname(os.path.abspath(__file__))
 dotenv_path = os.path.join(base_directory,'.env')  
@@ -33,7 +34,7 @@ def mean_pooling(model_output, attention_mask):
 def return_similar_articles(input_embedding):
     with conn:
         with conn.cursor() as cursor:
-            cursor.execute("select concat(article_name,' ',full_content), (%s::vector <=> c.embedding ) as cosine_distance from articles a join chunk c using(article_id) where (%s::vector <=> c.embedding )<=0.63 order by cosine_distance limit 15;",(input_embedding, input_embedding, ))
+            cursor.execute("select distinct concat(article_name,' ',full_content), penal_code_version, (%s::vector <=> c.embedding ) as cosine_distance from articles a join chunk c using(article_id) where (%s::vector <=> c.embedding )<=0.8 order by cosine_distance limit 15;",(input_embedding, input_embedding, ))
             rows = cursor.fetchall()
     return rows        
 
@@ -52,12 +53,37 @@ inputs = tokenizer(word_segmented_input, truncation=True, padding=True, return_t
 model_output = model(**inputs)
 final_embedding = mean_pooling(model_output, inputs['attention_mask'])[0].detach().tolist()
 
+# get all related articles
 all_related_articles=return_similar_articles(final_embedding)
 if(len(all_related_articles)==0):
     print("Không tìm thấy điều luật liên quan")
 else:
-    for article in all_related_articles:
-        print(article[0])
+    context_for_prompt = [article[0] for article in all_related_articles]
+    context_for_prompt_str = "\n".join(context_for_prompt)
+    context_for_prompt_str+=all_related_articles[0][1]  # add penal code version info
+
+    genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+    generation_config = {
+    "temperature": 0.2,   # 0.0 - 1.0 (Thấp = chính xác, Cao = sáng tạo)
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 10000, # Độ dài câu trả lời mong muốn
+    }
+    model = genai.GenerativeModel(
+    model_name="gemini-2.5-pro",
+    generation_config=generation_config
+    )
+
+    prompt = f"""
+    Bạn là 1 trợ lý am hiểu về luật pháp hãy giúp tôi tìm ra các chủ thể có tội và hình phạt tương ứng của họ trong trường hợp sau:
+    Ngữ cảnh: {input_text}
+    Biết các điều luật có thể liên quan đến trường hợp trong ngữ cảnh của tôi là:
+    {context_for_prompt_str}
+    Hãy liệt kê các chủ thể có tội và hình phạt tương ứng của họ không cần liệt kê ra các điều luật trong các điều luật liên quan được gửi nếu bạn cảm thấy điều luật đó thực sự không liên quan.
+    """
+    response = model.generate_content(prompt)
+    print(response.text)
+    
 
 
 
