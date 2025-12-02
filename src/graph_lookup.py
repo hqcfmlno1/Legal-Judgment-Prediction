61f675183dbd0370e873968e6baf73b2a775e646
Hoang Quoc Cuong <hoangquoccuong05th@gmail.com>
2025-12-02 08:58:31 +0700

    added class used for extract all the related articles

---
+++ src/graph_lookup.py
@@ -0,0 +1,25 @@
+import psycopg2
+import json
+class GraphLookUp:
+    def __init__(self, host, port, dbname, user, password, filepath):
+        self.conn = psycopg2.connect(
+            host=host,
+            port=port,
+            dbname=dbname,
+            user=user,
+            password=password,
+        )
+        self.filepath = filepath
+    def getRelatedArticle(self,article_id_list):
+        with open(self.filepath, 'r', encoding='utf-8') as f:
+            metadata = json.load(f)
+        all_related_article_list = [item2 for item1 in article_id_list for item2 in metadata[str(item1)]]
+        with self.conn:
+            with self.conn.cursor() as cursor:
+                cursor.execute(
+                    """
+                    select concat(article_id,' ',full_content) from articles where article_id in %s;
+                    """,
+                    (tuple(all_related_article_list),))
+                rows = cursor.fetchall()
+        return rows
