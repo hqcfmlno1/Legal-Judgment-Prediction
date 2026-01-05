d2ea37503afdc5e9139361a8384e50edda6930e1
Hoang Quoc Cuong <hoangquoccuong05th@gmail.com>
2026-01-05 19:24:06 +0700

    fix some config of llm

--- src/processed_input.py
+++ src/processed_input.py
@@ -7,12 +7,17 @@ import json
 from pydantic import BaseModel, Field
 from src import database
 from unidecode import unidecode
+import re
+from dotenv import load_dotenv
+import google.generativeai as genai
+
 
 class ProcessedInput:
 
     # for postgres web search
     def reformat(text):
         text = ViTokenizer.tokenize(text)
+        text = re.sub(r'[",.!?;]', '', text)
         text = unidecode(text)
         text = " or ".join(text.split())
         return text
@@ -28,4 +33,22 @@ class ProcessedInput:
         model_output = model(**inputs)
         final_embedding = ProcessedInput.mean_pooling(model_output, inputs['attention_mask'])
         return final_embedding[0].detach().tolist()
-
+    def queryRewriting(input_text,env_file_path):
+        load_dotenv(dotenv_path=env_file_path)
+        genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+        generation_config = {
+            "temperature": 0.2,   
+            "top_p": 0.6,
+            "top_k": 10,
+            "max_output_tokens": 10000, 
+        }
+        model = genai.GenerativeModel(
+            model_name="gemma-3-4b-it",
+            generation_config=generation_config
+        )
+        prompt = f"""
+            bạn là 1 trợ lý am hiểu về pháp luật việt nam hãy viết lại 1 câu có nghĩa tương tự với câu: {input_text} sử dụng các từ ngữ pháp lý của bộ luật hình sự việt nam
+            chú ý chỉ viết lại câu, không thêm bất kì thông tin nào, tuy nhiên có thể thêm ý kiến của bạn về việc tăng nặng giảm nhẹ
+            """
+        response = model.generate_content(prompt)
+        return response.text
